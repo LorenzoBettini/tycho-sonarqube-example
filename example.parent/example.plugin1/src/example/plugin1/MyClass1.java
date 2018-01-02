9f016d95838bd46a42bdefe8fd27806730dc18ec
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2018-01-02 09:16:17 +0100

    initial import

---
+++ example.parent/example.plugin1/src/example/plugin1/MyClass1.java
@@ -0,0 +1,12 @@
+package example.plugin1;
+
+public class MyClass1 {
+
+	public void myMethod1(String name) {
+		if (name != null) {
+			// this condition won't be tested
+			System.out.println("hello " + name);
+		}
+		System.out.println("hello");
+	}
+}
